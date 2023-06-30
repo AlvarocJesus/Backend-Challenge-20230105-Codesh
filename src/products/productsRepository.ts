0d16de811fa2dd4985c3ea39137274d625cf1963
AlvarocJesus <58888859+AlvarocJesus@users.noreply.github.com>
2023-06-30 18:58:30 -0300

    fix(products): Corrige query para atualizar dados

--- src/products/productsRepository.ts
+++ src/products/productsRepository.ts
@@ -21,9 +21,33 @@ export class ProductsRepository {
 
   async updateProduct(code: string, data: ProductsDTO): Promise<void> {
     try {
-      return await db('UPDATE products SET status = trash WHERE code = $1', [
-        code,
-      ]);
+      return await db(
+        'UPDATE products SET status=$2, imported_t=$3, url=$4, creator=$5, created_t=$6, last_modified_t=$7, product_name=$8, quantity=$9, brands=$10, categories=$11, labels=$12, cities=$13, purchase_places=$14, stores=$15, ingredients_text=$16, traces=$17, serving_size=$18, serving_quantity=$19, nutriscore_score=$20, nutriscore_grade=$21, main_category=$22, image_url=$23, WHERE code = $1',
+        [
+          code,
+          data.imported_t,
+          data.url,
+          data.creator,
+          data.created_t,
+          data.last_modified_t,
+          data.product_name,
+          data.quantity,
+          data.brands,
+          data.categories,
+          data.labels,
+          data.cities,
+          data.purchase_places,
+          data.stores,
+          data.ingredients_text,
+          data.traces,
+          data.serving_size,
+          data.serving_quantity,
+          data.nutriscore_score,
+          data.nutriscore_grade,
+          data.main_category,
+          data.image_url,
+        ]
+      );
     } catch (err: any) {
       throw new AppError(err);
     }
