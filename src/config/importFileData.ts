bb91412353b041fb9fc04e1b6dd3bf070baf8f97
AlvarocJesus <58888859+AlvarocJesus@users.noreply.github.com>
2023-06-30 15:22:56 -0300

    feat(importFileData): Adicionar busca dos arquivos e teste para retornar dados

--- src/config/importFileData.ts
+++ src/config/importFileData.ts
@@ -1,5 +1,24 @@
-import axios from "axios";
+async function teste() {
+  const res = await fetch(
+    'https://challenges.coode.sh/food/data/json/index.txt'
+  );
+  const text = await res.text();
+  const array = text.split('\n');
+  array.pop();
 
-const data = axios.get('');
+  const data = await fetch(
+    `https://challenges.coode.sh/food/data/json/${array[0]}`
+  );
+  // const json = await data.json();
+  // const texte = await data.text();
+  const arrayBuffer = await data.arrayBuffer();
+  console.log({ /* json, texte,*/ arrayBuffer });
 
-export default data;
+  /*  for await (const filename of array) {
+    const res = await fetch(
+      `https://challenges.coode.sh/food/data/json/${filename}`
+    );
+    const json = await res.json();
+  } */
+}
+teste();
