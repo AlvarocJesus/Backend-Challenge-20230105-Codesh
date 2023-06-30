6883e3430a2662a6861f840957660a55f130c9a7
AlvarocJesus <58888859+AlvarocJesus@users.noreply.github.com>
2023-06-30 15:41:30 -0300

    fix(productsDTO): Finaliza dto para validar dados

--- src/products/ProductsDTO.ts
+++ src/products/ProductsDTO.ts
@@ -1,26 +1,95 @@
-export class ProductsDTO{
-  /*  "code": 20221126,
-   "status": "published",
-   "imported_t": "2020-02-07T16:00:00Z",
-   "url": "https://world.openfoodfacts.org/product/20221126",
-   "creator": "securita",
-   "created_t": 1415302075,
-   "last_modified_t": 1572265837,
-   "product_name": "Madalenas quadradas",
-   "quantity": "380 g (6 x 2 u.)",
-   "brands": "La Cestera",
-   "categories": "Lanches comida, Lanches doces, Biscoitos e Bolos, Bolos, Madalenas",
-   "labels": "Contem gluten, Contém derivados de ovos, Contém ovos",
-   "cities": "",
-   "purchase_places": "Braga,Portugal",
-   "stores": "Lidl",
-   "ingredients_text": "farinha de trigo, açúcar, óleo vegetal de girassol, clara de ovo, ovo, humidificante (sorbitol), levedantes químicos (difosfato dissódico, hidrogenocarbonato de sódio), xarope de glucose-frutose, sal, aroma",
-   "traces": "Frutos de casca rija,Leite,Soja,Sementes de sésamo,Produtos à base de sementes de sésamo",
-   "serving_size": "madalena 31.7 g",
-   "serving_quantity": 31.7,
-   "nutriscore_score": 17,
-   "nutriscore_grade": "d",
-   "main_category": "en:madeleines",
-   "image_url": "https://static.openfoodfacts.org/images/products/20221126/front_pt.5.400.jpg"
- */
+import { IsNumber, IsOptional, IsString } from 'class-validator';
+
+export class ProductsDTO {
+  @IsNumber()
+  @IsOptional()
+  code?: number;
+
+  @IsString()
+  @IsOptional()
+  status?: string;
+
+  @IsString()
+  @IsOptional()
+  imported_t?: string;
+
+  @IsString()
+  @IsOptional()
+  url?: string;
+
+  @IsString()
+  @IsOptional()
+  creator?: string;
+
+  @IsNumber()
+  @IsOptional()
+  created_t?: number;
+
+  @IsNumber()
+  @IsOptional()
+  last_modified_t?: number;
+
+  @IsString()
+  @IsOptional()
+  product_name?: string;
+
+  @IsString()
+  @IsOptional()
+  quantity?: string;
+
+  @IsString()
+  @IsOptional()
+  brands?: string;
+
+  @IsString()
+  @IsOptional()
+  categories?: string;
+
+  @IsString()
+  @IsOptional()
+  labels?: string;
+
+  @IsString()
+  @IsOptional()
+  cities?: string;
+
+  @IsString()
+  @IsOptional()
+  purchase_places?: string;
+
+  @IsString()
+  @IsOptional()
+  stores?: string;
+
+  @IsString()
+  @IsOptional()
+  ingredients_text?: string;
+
+  @IsString()
+  @IsOptional()
+  traces?: string;
+
+  @IsString()
+  @IsOptional()
+  serving_size?: string;
+
+  @IsNumber()
+  @IsOptional()
+  serving_quantity?: number;
+
+  @IsNumber()
+  @IsOptional()
+  nutriscore_score?: number;
+
+  @IsString()
+  @IsOptional()
+  nutriscore_grade?: string;
+
+  @IsString()
+  @IsOptional()
+  main_category?: string;
+
+  @IsString()
+  @IsOptional()
+  image_url?: string;
 }
