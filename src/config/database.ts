bc7881c4b6127fcc7bb6c422657ba1b7d033265f
AlvarocJesus <58888859+AlvarocJesus@users.noreply.github.com>
2023-07-03 14:38:41 -0300

    Remove comentarios desnecessarios

--- src/config/database.ts
+++ src/config/database.ts
@@ -10,23 +10,3 @@ export const pool = new Client({
   password: process.env.DB_PASSWORD,
   port: 5432 || process.env.DB_PORT,
 });
-let databaseStatus: string;
-
-/* async function connect() {
-  await pool.connect();
-} */
-
-/* pool.on('connect', () => {
-  try {
-    console.log('Base de Dados conectado com sucesso!');
-    databaseStatus = 'database connected';
-  } catch (err) {
-    databaseStatus = 'database connected';
-  }
-}); */
-
-/* export default async function query(text?: any, params?: any){
-  await connect()
-  pool.query(text, params);
-} */
-
